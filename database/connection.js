5017e04f8db5ea4b367df3d9fe14e3e23de077ee
Abdallah-harb <abdallahabdelrahman186@gmail.com>
2025-02-20 14:51:21 +0200

    mongosh db

--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,35 @@
-const { MongoClient } = require('mongodb');
-const url = "mongodb+srv://abdallahabdelrahman186:Abdallah15@learn-mongo-db.74qhc.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db";
-const client = new MongoClient(url);
+const mongoose = require('mongoose');
+const url = "mongodb+srv://abdallahabdelrahman186:Abdallah15@learn-mongo-db.74qhc.mongodb.net/codeZone?retryWrites=true&w=majority&appName=learn-mongo-db";
 
-// Database Name
-const dbName = 'codeZone';
+mongoose.connect(url).then(()=>{
+    console.log('mongo db started');
+})
 
-async function main() {
-    await client.connect();
-    console.log('Connected successfully to server');
-    const db = client.db(dbName);
-    const collection = db.collection('courses');
-    const data = await collection.find().toArray();
-    console.log(data);
-}
 
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+module.exports = connectDB;
+
+
+
+
+
+        /*  try to interact with mongodb */
+// const { MongoClient } = require('mongodb');
+// const url = "mongodb+srv://abdallahabdelrahman186:Abdallah15@learn-mongo-db.74qhc.mongodb.net/?retryWrites=true&w=majority&appName=learn-mongo-db";
+// const client = new MongoClient(url);
+//
+// // Database Name
+// const dbName = 'codeZone';
+//
+// async function main() {
+//     await client.connect();
+//     console.log('Connected successfully to server');
+//     const db = client.db(dbName);
+//     const collection = db.collection('courses');
+//     const data = await collection.find().toArray();
+//     console.log(data);
+// }
+//
+// main()
+//     .then(console.log)
+//     .catch(console.error)
+//     .finally(() => client.close());
